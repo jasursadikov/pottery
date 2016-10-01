8569c984ee00ce38fe37fc3e18334e089784c716
Jasur Sadikov <jasur@sadikoff.com>
2016-10-01 19:45:11 +0500

    Waiting for LICENSE and README update. Save system implemented. Code refactored and documented.

--- Assets/Demo/Scripts/SavesManager.cs
+++ Assets/Demo/Scripts/SavesManager.cs
@@ -10,7 +10,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.UI;
